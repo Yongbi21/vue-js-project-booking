57a41b4e9bd7aebaa552fe46c665cea6949cb262
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2023-06-21 14:32:40 +0800

    fix ui

--- src/pages/Layout/MobileMenu.vue
+++ src/pages/Layout/MobileMenu.vue
@@ -1,30 +1,15 @@
 <template>
   <ul class="nav nav-mobile-menu">
     <li>
-      <md-field>
-        <label>Search</label>
-        <md-input v-model="search" type="text"></md-input>
-      </md-field>
-    </li>
-    <li>
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="material-icons">dashboard</i>
-        <p>Dashboard</p></a
-      >
-    </li>
-    <li>
       <drop-down>
         <a slot="title" class="dropdown-toggle" data-toggle="dropdown">
           <i class="material-icons">notifications</i>
-          <span class="notification">5</span>
+          <span class="notification">3</span>
           <p>Notifications</p>
         </a>
         <ul class="dropdown-menu dropdown-menu-right">
-          <li><a href="#">Mike John responded to your email</a></li>
-          <li><a href="#">You have 5 new tasks</a></li>
-          <li><a href="#">You're now friend with Andrew</a></li>
-          <li><a href="#">Another Notification</a></li>
-          <li><a href="#">Another One</a></li>
+          <li><a href="#">Kelly Kapoor responded to your email</a></li>
+          <li><a href="#">You have 3 new tasks</a></li>
         </ul>
       </drop-down>
     </li>
@@ -44,13 +29,8 @@ export default {
       selectedEmployee: null,
       employees: [
         "Jim Halpert",
-        "Dwight Schrute",
-        "Michael Scott",
-        "Pam Beesly",
         "Angela Martin",
         "Kelly Kapoor",
-        "Ryan Howard",
-        "Kevin Malone",
       ],
     };
   },
